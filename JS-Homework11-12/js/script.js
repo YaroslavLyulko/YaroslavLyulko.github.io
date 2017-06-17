cf806bd0654cd1a2620bc15af2024792463cfa6f
Yaroslav Lyulko <Yaroslav.Lyulko@silogic.com.ua>
2017-06-17 21:03:53 +0300

    Added js-hw11-12

--- JS-Homework11-12/js/script.js
+++ JS-Homework11-12/js/script.js
@@ -1,22 +1,52 @@
 $(document).ready(function() {
-
-$('.simple-carousel1').simpleCarousel();
+'use strict';
+$('.simple-carousel').simpleCarousel();
 
    
-var html = $('#item-tmpl').html();
+// var html = $('#item-tmpl').html();
 
-console.log(html);
-
-var data = {
-    title: "Some title"
+var userData = {
+    title: "User list created using a template",
+    items: [
+      {
+        name: 'Buck',
+        surname: 'Animal',
+        title: 'Actor',
+        address: 'Earth',
+        phoneNumber: '123-456-789',
+        email: 'Animal@hotmail.com',
+        photo: 'img/user1.jpg'
+      },
+      {
+        name: 'Girl',
+        surname: 'Hungry',
+        title: 'Child',
+        address: 'UK',
+        phoneNumber: '123-456-789',
+        email: 'Girl.bw@gb.com',
+        photo: 'img/user2.jpg'
+      },
+      {
+        name: 'Emerson',
+        surname: 'Loustau',
+        title: 'Front-end Developer',
+        address: 'India',
+        phoneNumber: '123-456-789',
+        email: 'E.Loustau@fed.com',
+        photo: 'img/user3.jpg'
+      }
+    ]
 };
+var parent = document.getElementById('container');
+var html = document.getElementById('item-tmpl').textContent;
+var hhh = _.template(html);
 
-console.log(data.title);
-
-var content = tmpl(html, data);
-
-$('body').append(content);
+console.log(hhh(userData));
+parent.innerHTML += hhh(userData);
 
 
+// var content = tmpl(html, data);
+
+// $('body').append(content1);
 
 });
